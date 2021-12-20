45f5e27f1b5f1fced2e544ac35de6b79e2ecf28f
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2021-12-20 06:43:28 +0100

    remove duplicate code

--- utils.py
+++ utils.py
@@ -91,26 +91,3 @@ def random_search(
             logging.error(traceback.format_exc())
 
     run.stop()
-
-# dataset
-size = 500
-dataset = datasets.uniform_N(10, size, 0)
-ground_truth = np.full(size, 10)
-dataset_name = f"{size}_uniform_10"
-
-# model
-model_fun = skdim.id.CorrInt
-model_name = "corrint"
-parameters_ranges = {
-        "k1": np.arange(5, 30),
-        "k2": np.arange(10, 50)
-}
-
-random_search(
-        model_fun,
-        dataset,
-        ground_truth,
-        parameters_ranges,
-        model_name,
-        dataset_name,
-        num_iter=10)
